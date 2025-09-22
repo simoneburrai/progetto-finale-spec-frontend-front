ea7a8c4759ab4df36287995ce1c8ec9d780a8c26
simoneburrai <burrai.s98@gmail.com>
2025-09-22 18:49:09 +0200

    feat: imported react router dom, returned broswer router in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ProductList from "./pages/ProductList"
 function App() {
 
-  return <>ciao</>
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/products" element={<ProductList/>}/>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,5 @@
+export default function ProductList(){
+    return <div>
+        <h1>Lista dei Prodotti:</h1>
+    </div>
+}
