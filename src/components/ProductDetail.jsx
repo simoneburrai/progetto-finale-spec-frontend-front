fd441de7f12b17aa213048dd803884337fd0a002
simoneburrai <burrai.s98@gmail.com>
2025-10-04 12:16:30 +0200

    fix: abled and disabled button favorites

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import { useProductContext } from "../contexts/ProductContext"
 
 export default function ProductDetail({product, addFavorite, comparedProducts}){
-    const {addCompared, removeCompared} = useProductContext();
+    const {addCompared, removeCompared, favoriteProducts} = useProductContext();
 
     const onChangeCompared = ()=>{
       if (comparedProducts.some(p => p.id === product.id)) {
@@ -13,14 +13,15 @@ export default function ProductDetail({product, addFavorite, comparedProducts}){
 
     }
 
-    
+    const disabledButton = favoriteProducts.some(p=> p.id === product.id) && "btn-outline-danger disabled" 
+    const abledButton = "btn-outline-success"
 
     return <div className="col-md-4">
             <div className="card h-100 shadow-sm">
               <div className="card-body text-center d-flex flex-column">
                 <h4 className="card-title"><Link className="text-decoration-none text-primary fw-bold" to={`/products/${product.id}`}>{product.title}</Link></h4>
                 <p className="text-muted mb-3">{product.category.toUpperCase()}</p>
-                <button className="btn btn-outline-success mt-auto" onClick={()=>addFavorite(product)}>Aggiungi ai Preferiti</button>
+                <button className={`btn  mt-auto ${disabledButton ? disabledButton : abledButton}`}onClick={()=>addFavorite(product)}>Aggiungi ai Preferiti</button>
                 <label>Confronta questo prodotto : <input 
                 checked={comparedProducts.some(p => p.id === product.id)} 
                 type="checkbox" 
